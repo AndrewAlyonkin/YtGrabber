40971c34667df3e4bdc355341008433ffcaba13e
AlenkinAndrew <oxqq@ya.ru>
2022-11-11 07:48:36 +0300

    Initial commit

---
+++ yt-dlp-bridge/src/main/java/com/afalenkin/ytdlpbridge/YtDlpBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.afalenkin.ytdlpbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtDlpBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtDlpBridgeApplication.class, args);
+	}
+
+}
